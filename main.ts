8d1f03110243738714416eee92ce8ee0c092c92d
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-06-12 15:38:14 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 microbitApp.compassButtonDown(CompassButtonOption.W, function () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    microbitApp.getSliderX(),
-    robotbit.Motors.M1B,
-    microbitApp.getSliderX() * -1
-    )
-    robotbit.MotorRunDual(
-    robotbit.Motors.M2B,
-    microbitApp.getSliderX() * -1,
-    robotbit.Motors.M2A,
-    microbitApp.getSliderX()
-    )
+    if (microbitApp.getToggleState(ToggleOption.Toggle1) == 0) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX(),
+        robotbit.Motors.M1B,
+        microbitApp.getSliderX() * -1
+        )
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M2A,
+        microbitApp.getSliderX()
+        )
+    } else {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX(),
+        robotbit.Motors.M1B,
+        microbitApp.getSliderX()
+        )
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M2A,
+        microbitApp.getSliderX() * -1
+        )
+    }
 })
 microbitApp.compassButtonDown(CompassButtonOption.N, function () {
     robotbit.MotorRunDual(
@@ -38,12 +53,21 @@ microbitApp.compassButtonDown(CompassButtonOption.NE, function () {
     )
 })
 microbitApp.compassButtonDown(CompassButtonOption.SE, function () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M2B,
-    microbitApp.getSliderX() * -1,
-    robotbit.Motors.M1B,
-    microbitApp.getSliderX() * -1
-    )
+    if (microbitApp.getToggleState(ToggleOption.Toggle1) == 0) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX() * -1
+        )
+    } else {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX(),
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX() * -1
+        )
+    }
 })
 microbitApp.compassButtonDown(CompassButtonOption.NW, function () {
     robotbit.MotorRunDual(
@@ -68,29 +92,51 @@ microbitApp.compassButtonDown(CompassButtonOption.S, function () {
     )
 })
 microbitApp.compassButtonDown(CompassButtonOption.E, function () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    microbitApp.getSliderX() * -1,
-    robotbit.Motors.M1B,
-    microbitApp.getSliderX()
-    )
-    robotbit.MotorRunDual(
-    robotbit.Motors.M2B,
-    microbitApp.getSliderX(),
-    robotbit.Motors.M2A,
-    microbitApp.getSliderX() * -1
-    )
+    if (microbitApp.getToggleState(ToggleOption.Toggle1) == 0) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M1B,
+        microbitApp.getSliderX()
+        )
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX(),
+        robotbit.Motors.M2A,
+        microbitApp.getSliderX() * -1
+        )
+    } else {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M1B,
+        microbitApp.getSliderX() * -1
+        )
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX(),
+        robotbit.Motors.M2A,
+        microbitApp.getSliderX()
+        )
+    }
 })
 microbitApp.compassButtonDown(CompassButtonOption.SW, function () {
-    robotbit.MotorRunDual(
-    robotbit.Motors.M1A,
-    microbitApp.getSliderX() * -1,
-    robotbit.Motors.M2A,
-    microbitApp.getSliderX() * -1
-    )
+    if (microbitApp.getToggleState(ToggleOption.Toggle1) == 0) {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M2A,
+        microbitApp.getSliderX() * -1
+        )
+    } else {
+        robotbit.MotorRunDual(
+        robotbit.Motors.M2B,
+        microbitApp.getSliderX() * -1,
+        robotbit.Motors.M1A,
+        microbitApp.getSliderX()
+        )
+    }
 })
-microbitApp.setLightState(LightOption.Light1, LightState.On)
-microbitApp.setLightState(LightOption.Light2, LightState.On)
 basic.forever(function () {
 	
 })
